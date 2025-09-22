13ec901270074455a0fb765edb9e4252704f30bc
Александр Швецов <shvitc@yandex.ru>
2025-09-22 22:41:29 +0300

    chore: setup Material-UI integration with Next.js

--- next_frontend/src/app/layout.tsx
+++ next_frontend/src/app/layout.tsx
@@ -1,6 +1,17 @@
 import {Metadata} from "next";
 import "./globals.css";
 import Layout from "@/components/layout/Layout";
+import {AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
+import {Roboto} from "next/font/google";
+import {ThemeProvider} from "@mui/material/styles";
+import theme from "@/theme_mui/theme";
+
+const roboto = Roboto({
+    weight: ["300", "400", "500", "700"],
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--font-roboto",
+});
 
 export const metadata: Metadata = {
     title: {
@@ -15,9 +26,13 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="ru">
+        <html lang="ru" className={roboto.variable}>
             <body>
-                <Layout>{children}</Layout>
+                <AppRouterCacheProvider>
+                    <ThemeProvider theme={theme}>
+                        <Layout>{children}</Layout>
+                    </ThemeProvider>
+                </AppRouterCacheProvider>
             </body>
         </html>
     );
