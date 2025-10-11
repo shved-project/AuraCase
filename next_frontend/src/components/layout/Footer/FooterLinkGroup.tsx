fe7e8f3c1bcae72b272ec1f8bad0d5c4961acd3e
Александр Швецов <shvitc@yandex.ru>
2025-10-11 21:31:44 +0300

    feat: implement responsive footer layout

--- next_frontend/src/components/layout/Footer/FooterLinkGroup.tsx
+++ next_frontend/src/components/layout/Footer/FooterLinkGroup.tsx
@@ -1,5 +1,5 @@
 import {FooterLinksGroup} from "@/constants/footer_links";
-import {IconButton, Stack, Typography} from "@mui/material";
+import {Box, IconButton, Stack, Typography} from "@mui/material";
 import Link from "next/link";
 
 interface Props {
@@ -20,49 +20,51 @@ const FooterLinkGroup = ({group}: Props) => {
             >
                 {group.title}
             </Typography>
-            <ul className={`${group.type === "buttonLink" && "flex-row"} flex flex-col gap-2 ml-3`}>
-                {group.type === "link" &&
-                    group.links.map((link, index) => (
-                        <li key={index}>
-                            <Link
-                                href={link.href}
-                                className="relative after:content-[''] after:block after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 after:bg-current after:duration-150 hover:after:w-full"
-                            >
-                                {link.label}
-                            </Link>
-                        </li>
-                    ))}
-                {group.type === "iconLink" &&
-                    group.links.map((link, index) => {
-                        const Icon = link.icon;
-
-                        return (
+            <Box sx={{marginLeft: {xs: 1.5, sm: 0}}}>
+                <ul className={`${group.type === "buttonLink" && "flex-row"} flex flex-col gap-2`}>
+                    {group.type === "link" &&
+                        group.links.map((link, index) => (
                             <li key={index}>
-                                <a
+                                <Link
                                     href={link.href}
-                                    className="flex items-start gap-1 max-w-max relative after:content-[''] after:block after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 after:bg-current after:duration-150 hover:after:w-full"
+                                    className="relative after:content-[''] after:block after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 after:bg-current after:duration-150 hover:after:w-full"
                                 >
-                                    <Icon />
                                     {link.label}
-                                </a>
+                                </Link>
                             </li>
-                        );
-                    })}
-                {group.type === "buttonLink" &&
-                    group.links.map((link, index) => {
-                        const Icon = link.icon;
+                        ))}
+                    {group.type === "iconLink" &&
+                        group.links.map((link, index) => {
+                            const Icon = link.icon;
 
-                        return (
-                            <li key={index}>
-                                <a href={link.href} target="_blank" aria-label={link.ariaLabel}>
-                                    <IconButton color="inherit">
+                            return (
+                                <li key={index}>
+                                    <a
+                                        href={link.href}
+                                        className="flex items-start gap-1 max-w-max relative after:content-[''] after:block after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-0 after:bg-current after:duration-150 hover:after:w-full"
+                                    >
                                         <Icon />
-                                    </IconButton>
-                                </a>
-                            </li>
-                        );
-                    })}
-            </ul>
+                                        {link.label}
+                                    </a>
+                                </li>
+                            );
+                        })}
+                    {group.type === "buttonLink" &&
+                        group.links.map((link, index) => {
+                            const Icon = link.icon;
+
+                            return (
+                                <li key={index}>
+                                    <a href={link.href} target="_blank" aria-label={link.ariaLabel}>
+                                        <IconButton color="inherit">
+                                            <Icon />
+                                        </IconButton>
+                                    </a>
+                                </li>
+                            );
+                        })}
+                </ul>
+            </Box>
         </Stack>
     );
 };
--- next_frontend/src/components/layout/Footer/index.tsx
+++ next_frontend/src/components/layout/Footer/index.tsx
@@ -9,8 +9,13 @@ const Footer = () => {
     return (
         <Box component="footer" sx={{backgroundColor: "primary.main", color: "#fff"}}>
             <Container>
-                <Stack>
-                    <Stack direction="row" alignItems="center" justifyContent="space-between">
+                <Stack direction={{sm: "row"}} justifyContent={{sm: "space-between"}} gap={{xs: 4, sm: 15}}>
+                    <Stack
+                        direction={{xs: "row", sm: "column"}}
+                        alignItems={{xs: "center", sm: "flex-start"}}
+                        justifyContent={{xs: "space-between", sm: "flex-start"}}
+                        spacing={{sm: 2}}
+                    >
                         <Logo size="large" />
                         <Link href="/products">
                             <Button variant="contained" color="secondary" size="large">
@@ -18,13 +23,16 @@ const Footer = () => {
                             </Button>
                         </Link>
                     </Stack>
-                    <Stack sx={{marginTop: 4}} spacing={3}>
+                    <Stack gap={3} direction={{sm: "row"}} sx={{flexWrap: {xs: "nowrap", sm: "wrap"}}}>
                         {FOOTER_LINKS.map((item, index) => (
                             <FooterLinkGroup key={index} group={item} />
                         ))}
                     </Stack>
                 </Stack>
             </Container>
+            <Box sx={{textAlign: "center", backgroundColor: "#000", paddingY: 0.5}}>
+                © 2025 AuraCase. Все права защищены.
+            </Box>
         </Box>
     );
 };
